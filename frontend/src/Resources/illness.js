18c43fd0a75d61f556017380591a68ef07b72616
Adi-Singhal <adityasinghal.sre@gmail.com>
2023-04-22 14:32:11 +0530

    add illness data

---
+++ frontend/src/Resources/illness.js
@@ -0,0 +1,43 @@
+const mentalIllness = {
+    "Generalised Anxiety Disorder": {
+        "Symptoms": ["Restlessness", "Fatigue", "Irratability", "Difficulty sleeping", "Impaired concentration"],
+        "Details": "Generalised anxiety is excessive anxiety and constant worry about many things. The focus of the anxiety might be family or friends, health, work, money or forgetting important appointments."
+        
+    },
+    "Social Phobia":{
+        "Symptoms":["Blushing","Feeling of mind going blank","Muscle Tension","Quickened Heartbeat","Upset Stomach","Trembling","Dizziness"],
+        "Details": "Social phobia is a type of anxiety disorder. People who have social phobia experience extreme and persistent anxiety associated with social or performance situations."
+    },
+    "Panic Disorders":{
+        "Symptoms":["Trembling","Numbness","Hyperventilation","Heart Palpitations","Chills or Hot flashes"],
+        "Details": "Panic disorder is an anxiety disorder where you regularly have sudden attacks of panic or fear. Everyone experiences feelings of anxiety and panic at certain times. It's a natural response to stressful or dangerous situations."
+    },
+    "Obsessive compulsive disorder":{
+        "Symptoms":["Organization","Perfectionism","Attention to detail","Frequent list making","Rigidity","Devotion to work","Social Isolation"],
+        "Details": "Obsessive-compulsive disorder (OCD) is a common, chronic, and long-lasting disorder in which a person has uncontrollable, reoccurring thoughts."
+    },
+    "Post-traumatic stress disorder (PTSD)":{
+        "Symptoms":["Fear","Inablity to bounce back and trust others","Inpulsivity","Depression","Constant Anger","Daydreaming","Shutting Down"],
+        "Details": "Post-traumatic stress disorder (PTSD) is a disorder that develops in some people who have experienced a shocking, scary, or dangerous event."
+    },
+    "Oppositional defiant disorder (ODD)":{
+        "Symptoms":["Losing Temper","Frequent Outbursts","Frequently disrespectful","Argumentive","Defiant behavior","Spiteful","Seeking Revenge"],
+        "Details":"Even the best-behaved children can be difficult and challenging at times. But oppositional defiant disorder (ODD) includes a frequent and ongoing pattern of anger, irritability, arguing and defiance toward parents and other authority figures."
+    },
+    "Conduct disorder (CD)":{
+        "Symptoms":["Initiates Physicial Fights","Using weapons","Harming people and animals","Bullying others"],
+        "Details":"Conduct Disorder (CD) is diagnosed when children show an ongoing pattern of aggression toward others, and serious violations of rules and social norms at home, in school, and with peers."
+    },
+    "Depression":{
+        "Symptoms":["Anxiety","Hopelessness","Mode Swings","Change in body weight","Feel low","Sucidial Tendancy"],
+        "Details":"Depression is a mood disorder characterised by lowering of mood, loss of interest and enjoyment, and reduced energy. It is not just feeling sad."
+    },
+    "Paranoia":{
+        "Symptoms":["Increased Isolation","Mistrust of others","Hypervigilance","Stubborness","Hostility"],
+        "Details":"Paranoia is the irrational and persistent feeling that people are ‘out to get you’. Paranoia may be a symptom of conditions including paranoid personality disorder, delusional (paranoid) disorder and schizophrenia."
+    },
+    "Psychosis":{
+        "Symptoms":["Delusions","Suspiciousness","Sleep Difficulties","Social Withdrawal","Hallucinations"],
+        "Details":"Psychosis refers to a collection of symptoms that affect the mind, where there has been some loss of contact with reality."
+    },
+}
