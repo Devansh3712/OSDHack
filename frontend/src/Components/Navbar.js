8e0fe7184463fd34f09f3fd4672c5dada9ba73cf
barbieagrawal <barbieagrawal1912@gmail.com>
2023-04-22 14:04:36 +0530

    css modified

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -8,8 +8,6 @@ const Navbar = () => {
                     <ul>
                         <li><Link to="/">Home</Link></li>
                         <li><Link to="/">Articles</Link></li>
-                        <li><Link to="/">Relax</Link></li>
-                        <li><Link to="/">For You</Link></li>
                         <li><Link to="/login">Login</Link></li>
                         <li><Link to="/register">Register</Link></li>
                     </ul>
