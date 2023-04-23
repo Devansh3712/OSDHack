bd5501b4be34a6e4ba0890b6f80dc9d9e39cbc4b
Rahul <rahulkalra045@gmail.com>
2023-04-23 13:21:45 +0530

    Helplines added

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -4,18 +4,18 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
     return (
         <div>
-            <section style={{display:"flex"}}>
-                <div className= "logo" style={{display:"flex",width:"60px",height:"60px"}}>
-                <img src={logo} alt="..." />
+            <section style={{ display: "flex" }}>
+                <div className="logo" style={{ display: "flex", width: "60px", height: "60px" }}>
+                    <img src={logo} alt="..." />
                 </div>
                 <nav className="stroke">
                     <ul>
                         <li><Link to="/">Home</Link></li>
                         <li><Link to="/articles">Articles</Link></li>
-                        <li><Link to="/relax">Relax</Link></li>
                         <li><Link to="/for you">For You</Link></li>
                         <li><Link to="/login">Login</Link></li>
                         <li><Link to="/register">Register</Link></li>
+                        <li><Link to="/helpline">Helplines</Link></li>
                     </ul>
                 </nav>
             </section>
