cb9a531f2289ecba939902d4ac935c50c1108274
Adi-Singhal <adityasinghal.sre@gmail.com>
2023-04-23 14:35:01 +0530

    create test links

---
+++ frontend/src/Resources/test.js
@@ -0,0 +1,12 @@
+const tests = {
+    "Depression Test": "https://screening.mhanational.org/screening-tools/depression/?ref",
+    "Anxiety Test": "https://screening.mhanational.org/screening-tools/anxiety/?ref",
+    "Psychosis & Schizophrenia Test": "https://screening.mhanational.org/screening-tools/psychosis/?ref",
+    "PTSD Test": "https://screening.mhanational.org/screening-tools/ptsd/?ref",
+    "Eating Disorder Test": "https://screening.mhanational.org/screening-tools/eating-disorder/?ref",
+    "Addiction Test": "https://screening.mhanational.org/screening-tools/addiction/?ref",
+    "Parent Test: Your Child’s Mental Health":"https://screening.mhanational.org/screening-tools/parent/?ref",
+    "Bipolar Test":"https://screening.mhanational.org/screening-tools/bipolar/?ref",
+    "Youth Mental Health Test":"https://screening.mhanational.org/screening-tools/youth/?ref",
+    "ADHD (Attention Deficient Hyperactivity Disorder":"https://screening.mhanational.org/screening-tools/adhd/?ref",
+}
